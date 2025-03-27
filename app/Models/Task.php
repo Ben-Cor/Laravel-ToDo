edcf3bc00f2c8238fec305dd4d81ee2530381758
Ben Cornell <ben_cornell@hotmail.co.uk>
2025-03-27 10:57:52 +0000

    relationships setup

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,11 +2,23 @@
 
 namespace App\Models;
 
+use Database\Factories\TaskFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Task extends Model
 {
-    /** @use HasFactory<\Database\Factories\TaskFactory> */
+    /** @use HasFactory<TaskFactory> */
     use HasFactory;
+
+    public function categories() {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function user(): BelongsToMany
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
