0a89684e781cfd48ec8f01ec7001d79702c44916
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-04-03 10:26:23 +0100

    method and route for updating a task created

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -26,4 +26,23 @@ class TaskController extends Controller
             'message' => 'Task successfully created',
         ], 201);
     }
+
+    public function updateTask(TaskRequest $request, Task $task)
+    {
+        $task->content = $request->input('content');
+        $task->completed = $request->input('completed', false);
+        $task->user_id = $request->input('user_id');
+        $task->due_date = $request->input('due_date');
+        $task->save();
+
+        if (! $task->save()) {
+            return response()->json([
+                'message' => 'Task update failed',
+            ], 500);
+        }
+
+        return response()->json([
+            'message' => 'Task successfully updated',
+        ], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/users', [UserController::class, 'all']);
 Route::get('/users/{user}', [UserController::class, 'find']);
 Route::post('/tasks', [TaskController::class, 'addTask']);
+Route::put('/tasks/{task}', [TaskController::class, 'updateTask']);
