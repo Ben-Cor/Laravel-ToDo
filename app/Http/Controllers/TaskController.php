459097d0f6fba429d8eb41a5aacb8f2242ec8d52
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-04-03 09:48:18 +0100

    basic add task method created with route

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,7 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
+
 class TaskController extends Controller
 {
-    //
+    public function addTask()
+    {
+        $task = request()->validate([
+            'content' => 'required|string|max:255',
+            'completed' => 'boolean',
+            'user_id' => 'required|exists:users,id',
+            'due_date' => 'nullable|date',
+        ]);
+
+        $task = Task::create($task);
+
+        return response()->json([
+            'message' => 'Task successfully created',
+            'data' => $task,
+        ], 201);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/users', [UserController::class, 'all']);
 Route::get('/users/{user}', [UserController::class, 'find']);
+Route::post('/tasks', [TaskController::class, 'addTask']);
