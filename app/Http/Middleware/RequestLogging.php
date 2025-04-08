ce56414f34228b7eefeb7b1de95104a8487a91a9
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-04-08 15:18:12 +0100

    middleware logic added and middleware added to app.php

--- app/Http/Middleware/RequestLogging.php
+++ app/Http/Middleware/RequestLogging.php
@@ -4,17 +4,27 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class RequestLogging
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request);
+        $response = $next($request);
+
+        if ($response->getStatusCode() === 404) {
+            Log::info('404', [
+                'method' => $request->method(),
+                'url' => $request->fullUrl(),
+            ]);
+        } else {
+            Log::channel('requests')->info('Requests', [
+                'method' => $request->method(),
+                'url' => $request->fullUrl(),
+            ]);
+        }
+
+        return $response;
     }
 }
