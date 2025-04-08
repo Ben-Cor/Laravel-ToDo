330e08593f0a9741762948bb964cfaf703bab747
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-04-08 11:38:07 +0100

    user update method and route added

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ Route::get('/users', [UserController::class, 'all']);
 Route::get('/users/{user}', [UserController::class, 'find']);
 Route::post('/users', [UserController::class, 'add']);
 Route::delete('/users/{user}', [UserController::class, 'delete']);
+Route::put('/users/{user}', [UserController::class, 'update']);
 Route::post('/tasks', [TaskController::class, 'addTask']);
 Route::put('/tasks/{task}', [TaskController::class, 'updateTask']);
 Route::delete('/tasks/{task}', [TaskController::class, 'deleteTask']);
